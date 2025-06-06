77f6e7b5ef29031b1601420ab6344870be92a4cd
laboratorys <359916450@qq.com>
2025-06-06 17:17:38 +0800

    Update

--- configure.sh
+++ configure.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 run_backup(){
-  b_path="download/${BAK_VERSION-"latest"}"
-  if [ "${BAK_VERSION-"latest"}" = "latest" ]; then
-    b_path="latest/download"
+  if [[ -n "$BAK_APP_NAME" || -n "${BAK_APP_NAME}" ]]; then
+      b_path="download/${BAK_VERSION-"latest"}"
+       if [ "${BAK_VERSION-"latest"}" = "latest" ]; then
+         b_path="latest/download"
+       fi
+       curl -s -L "https://github.com/laboratorys/backup2gh/releases/${b_path}/backup2gh-linux-amd64.tar.gz" -o backup2gh.tar.gz \
+           && tar -xzf backup2gh.tar.gz \
+           && rm backup2gh.tar.gz \
+           && chmod +x backup2gh
+       export WEB_PATH=/backup2gh
+       nohup /app/backup2gh > /dev/null 2>&1 &
   fi
-  curl -s -L "https://github.com/laboratorys/backup2gh/releases/${b_path}/backup2gh-linux-amd64.tar.gz" -o backup2gh.tar.gz \
-      && tar -xzf backup2gh.tar.gz \
-      && rm backup2gh.tar.gz \
-      && chmod +x backup2gh
-  export WEB_PATH=/backup2gh
-  nohup /app/backup2gh > /dev/null 2>&1 &
+
 }
 run_dashboard(){
   d_path="download/${NZ_VERSION-"latest"}"
